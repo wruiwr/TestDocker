6c5ffe6881c27230047ff14d70002b2fd97e4a35
wruiwr <wruiwr86@gmail.com>
2016-06-04 02:47:32 +0200

    Create A.java

---
+++ jsrc/A.java
@@ -0,0 +1,15 @@
+package hib.dat100.testassignment;
+
+public class A {
+
+	public int inc (int i) {
+		return i + 1;
+	}
+	
+	public int dec (int i) {
+		// TODO - implement this method
+		// currently working on this
+		return 0;
+	}
+	
+}
