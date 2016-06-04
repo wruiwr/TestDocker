3849dd32792fd6c645add7e83b9feda926e64c77
wruiwr <wruiwr86@gmail.com>
2016-06-04 02:47:03 +0200

    Create TestA.java

---
+++ jsrc/TestA.java
@@ -0,0 +1,20 @@
+package hib.dat100.testassignment;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestA {
+
+	@Test
+	public void testinc() {
+		A a = new A();
+		assertEquals(5,a.inc(4));
+	}
+	
+	@Test
+	public void testdec() {
+		A a = new A();
+		assertEquals(3,a.dec(4));
+	}
+}
