803215fc2d3ac95d61a11405e267d5f7f08f1592
wruiwr <wruiwr86@gmail.com>
2016-06-05 17:17:26 +0200

    Update TestRunner.java

--- jsrc/TestRunner.java
+++ jsrc/TestRunner.java
@@ -6,8 +6,10 @@ public class TestRunner {
    public static void main(String[] args) {
       Result result = JUnitCore.runClasses(TestA.class);
       for (Failure failure : result.getFailures()) {
-         System.out.println(failure.toString());
+         System.out.println("The failed test: " + failure.toString());
       }
-      System.out.println(result.wasSuccessful());
+      System.out.println("The number of tests run: " + result.getRunCount());
+      System.out.println("The number of tests that failed during the run: " + result.getFailureCount());
+      System.out.println("The result of tests: " + result.wasSuccessful());
    }
 } 
